de33529651401b961b08871151cadb812c8ac9f6
adrianbparra <adrianplane12@gmail.com>
2019-11-13 22:28:06 -0700

    Was able to break out parts to make smaller components.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-function App() {
+function App(props) {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  
   //You'll need one for the home score and another for the away score.
 
   const [homeScore, setHome] = useState(0);
 
   const [awayScore, setAway] = useState(0);
+
+  const [quarter, setQuarter] = useState(0);
+
+
 
   const touchdownHome = event => {
     setHome(homeScore + 7);
@@ -27,30 +31,39 @@ function App() {
     setAway(awayScore + 1);
   };
 
+  const addQuarter = (e) =>{
+    if(quarter < 4) {
+      setQuarter(quarter + 1);
+    } else {
+      setQuarter(quarter = 1);
+    };
+  };
+
+
+  // const AddQuarter = () => {
+  //   return (
+  //     <div className="quarterButton">{quarter}</div>
+  //   )
+  // };
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
+          <Home home = {homeScore}/>
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+          <Away away = {awayScore}/>
         </div>
-        <BottomRow />
+        <BottomRow quarter = {quarter}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick = {touchdownHome}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick = {fieldGoalHome}>Home Field Goal</button>
+        </div>
+        <div className="quarterButton">
+          <button className="quarterButtons" onClick = {addQuarter}>Add a Quarter</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick = {touchdownAway}>Away Touchdown</button>
@@ -60,5 +73,28 @@ function App() {
     </div>
   );
 }
+
+function Home(props){
+  return(
+    <div className="home">
+      <h2 className="home__name">Lions</h2>
+
+      {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+      <div className="home__score">{props.home}</div>
+    </div>
+  )
+}
+
+function Away(props){
+  return(
+    <div className="away">
+      <h2 className="away__name">Tigers</h2>
+      <div className="away__score">{props.away}</div>
+    </div>
+  )
+
+}
+
 
 export default App;
